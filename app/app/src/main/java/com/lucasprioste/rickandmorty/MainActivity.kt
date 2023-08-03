4140beaa2c98ebc1fcb63f091c969bf122a3ce97
Lucas Prioste <lucas.prioste@mobiweb.pt>
2023-08-03 14:20:50 +0100

    Colors

--- app/app/src/main/java/com/lucasprioste/rickandmorty/MainActivity.kt
+++ app/app/src/main/java/com/lucasprioste/rickandmorty/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.lucasprioste.rickandmorty.ui.theme.RickAndMortyTheme
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
@@ -23,6 +24,10 @@ class MainActivity : ComponentActivity() {
         }
         setContent {
             RickAndMortyTheme {
+                val systemUiController = rememberSystemUiController()
+                systemUiController.setSystemBarsColor(
+                    color = MaterialTheme.colors.primary
+                )
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
                     Greeting("Android")
